523fdd48bdb126fb108c11fb452412f6fed0fb63
Hille <hille@onsive.net>
2021-03-17 23:01:36 +0000

    Changed variable names

--- hetznerddns.sh
+++ hetznerddns.sh
@@ -4,8 +4,8 @@ set -e;
 ipv4Regex="((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])"
 
 # DSM Config
-username="$1"
-password="$2"
+recordId="$1"
+accessToken="$2"
 #hostname="$3"
 ipAddr="$4"
 
@@ -15,8 +15,8 @@ else
     recordType="AAAA";
 fi
 
-res=$(curl "https://dns.hetzner.com/api/v1/records/$username" \
-     -H "Auth-API-Token: $password")
+res=$(curl "https://dns.hetzner.com/api/v1/records/$recordId" \
+     -H "Auth-API-Token: $accessToken")
 recordIp=$(echo "$res" | jq -r ".record.value")
 
 if [[ $recordIp = "$ipAddr" ]]; then
@@ -27,12 +27,12 @@ fi
 name=$(echo "$res" | jq -r ".record.name")
 zone_id=$(echo "$res" | jq -r ".record.zone_id")
 
-res=$(curl -X "PUT" "https://dns.hetzner.com/api/v1/records/$username" \
+res=$(curl -X "PUT" "https://dns.hetzner.com/api/v1/records/$recordId" \
      -H "Content-Type: application/json" \
-     -H "Auth-API-Token: $password" \
+     -H "Auth-API-Token: $accessToken" \
      -d "{
   \"value\": \"$ipAddr\",
-  \"ttl\": 300,
+  \"ttl\": 600,
   \"type\": \"$recordType\",
   \"name\": \"$name\",
   \"zone_id\": \"$zone_id\"
